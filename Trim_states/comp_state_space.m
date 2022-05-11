25378bcc4047c43e6ecda3b6cda15577a4da05f5
SurajRajawat <79207907+SurajRajawat@users.noreply.github.com>
2022-05-11 17:58:27 +0530

    Add files via upload

--- Trim_states/comp_state_space.m
+++ Trim_states/comp_state_space.m
@@ -5,21 +5,22 @@ function [A_lon,B_lon,A_lat,B_lat, A, B] = comp_state_space(filename,x_trim,u_tr
   
   
     [A,B,C,D]=linmod(filename,x_trim,u_trim);
+    % similarity transformation
+    T_lat = zeros(5,12);
+    T_lat(1,5) = 1;
+    T_lat(2,10) = 1;
+    T_lat(3,12) = 1;
+    T_lat(4,7) = 1;
+    T_lat(5,9) = 1;
+    A_lat = T_lat*A*T_lat';
+    B_lat = T_lat*B;
+    T_lat
 
-    P_lat = zeros(5,12);
-    P_lat(1,5) = 1;
-    P_lat(2,10) = 1;
-    P_lat(3,12) = 1;
-    P_lat(4,7) = 1;
-    P_lat(5,9) = 1;
-    A_lat = P_lat*A*P_lat';
-    B_lat = P_lat*B;
-
-    P_lon = zeros(5,12);
-    P_lon(1,4) = 1;
-    P_lon(2,6) = 1;
-    P_lon(3,11) = 1;
-    P_lon(4,8) = 1;
-    P_lon(5,3) = -1;
-    A_lon = P_lon*A*P_lon';
-    B_lon = P_lon*B;
+    T_lon = zeros(5,12);
+    T_lon(1,4) = 1;
+    T_lon(2,6) = 1;
+    T_lon(3,11) = 1;
+    T_lon(4,8) = 1;
+    T_lon(5,3) = 1;
+    A_lon = T_lon*A*T_lon';
+    B_lon = T_lon*B;
